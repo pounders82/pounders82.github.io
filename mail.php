c869bb05f5e278183a14289f35f3ba409f0e7d99
Nick Pounders <nickpounders@gmail.com>
2019-02-16 17:21:29 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -29,8 +29,10 @@
 		$recipient = "nickpounders@gmail.com";
 		
 		$mailheader = "From: $email \r\n";
+
+		mail($recipient, $name, $formcontent, $mailheader);
 		
-		if( mail($recipient, $name, $formcontent, $mailheader)){
+		if( mail($recipient, $name, $formcontent, $mailheader) == false ){
 			$data['error'] = 'Sorry, an error occured!';
 		}else{
 			$data['error'] = false;
